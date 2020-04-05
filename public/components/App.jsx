433802a438053e1b6d7cfc1693a144126f7cd3e3
Jesse Bell <jkurtasbell@gmail.com>
2020-04-05 00:15:42 -0700

    Add handle Functions To App.jsx

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function App ()  {
+const  [subject, setSubject] = useState('');
+const  [body, setBody] = useState('');
+let sub = '';
 
+  const handleSubjectChange = (e) => {
+    e.preventDefault();
+    setSubject(e.target.value)
+  }
+
+  const handleBodyChange = (e) => {
+    e.preventDefault();
+    setBody(e.target.value)
+    console.log('body', body)
+  }
 
   return (
       <div>
-        <div>
-          <p>MeAngry</p>
-        </div>
       <form action="/send" method="POST">
-          <input type="text" className="subject" placeholder="Subject"/>
+          <input type="text" className="subject" placeholder="Subject" onChange={handleSubjectChange}/>
           <br/>
-          <input type="text" className="body"/>
+          <input type="text" className="body" onChange={handleSubjectChange}/>
           <br/>
           <button type="submit">SEND</button>
         </form>

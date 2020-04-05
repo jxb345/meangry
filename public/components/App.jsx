c04e1c9f5420a7465feb15186ab0934ba8c7894f
Jesse Bell <jkurtasbell@gmail.com>
2020-04-05 00:02:59 -0700

    Show Basic Styling

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -6,12 +6,14 @@ function App ()  {
 
   return (
       <div>
+        <div>
+          <p>MeAngry</p>
+        </div>
       <form action="/send" method="POST">
-          <input type="text" placeholder="Subject"/>
-          <label >
-            BODY
-          </label>
+          <input type="text" className="subject" placeholder="Subject"/>
+          <br/>
           <input type="text" className="body"/>
+          <br/>
           <button type="submit">SEND</button>
         </form>
       </div>

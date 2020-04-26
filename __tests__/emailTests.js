2d6a426616bd89c62f92fcdfadbe508510ca5247
Jesse Bell <jkurtasbell@gmail.com>
2020-04-25 22:59:58 -0700

    Remove enzyme And Add react-testing-library

--- __tests__/emailTests.js
+++ __tests__/emailTests.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from '../public/components/App.jsx';
 
 describe("App", () => {

36b80f80003f051341449cdb6f19722e7508af67
Jesse Bell <jkurtasbell@gmail.com>
2020-05-13 15:31:10 -0700

    Add identifier Field To Db

--- backend/emailsDbConnect.js
+++ backend/emailsDbConnect.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/emailAddresses');
+const { v4: uuidv4 } = require('uuid');
+
 
 let emailSchema = mongoose.Schema({
   id: Number,
+  identifier: String,
   email: String,
   numSent: Number,
   verified: Boolean
@@ -11,7 +14,9 @@ let emailSchema = mongoose.Schema({
 let Email = mongoose.model('Email', emailSchema);
 
 const saveEmail = (email, cb) => {
-  Email.create({ email: email, numSent: 0, verified: false }, (err) => {
+  let generateUnique = uuidv4();
+  console.log('generateUnique', generateUnique)
+  Email.create({ identifier: generateUnique, email: email, numSent: 0, verified: false }, (err) => {
     if (err) { throw err; }
     console.log('email saved !!!');
     cb();

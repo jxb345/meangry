4433f6b1f976cdb82372a97aee210034ae4155db
Jesse Bell <jkurtasbell@gmail.com>
2020-11-19 14:47:40 -0800

    Tidy Up styles.css

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,74 @@
-
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
-const express = require('express');
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const bodyParser = require('body-parser')
-const { send } = require('./sendEmail.js');
-const { selectEmailAddress, numOfUsers, verifyEmailAddress, removeEmailAddress } = require('./models.js');
-const {  saveEmail } = require('./emailsDbConnect.js')
+const bodyParser = require("body-parser");
+const { send } = require("./sendEmail.js");
+const {
+  selectEmailAddress,
+  numOfUsers,
+  verifyEmailAddress,
+  removeEmailAddress,
+} = require("./models.js");
+const { saveEmail } = require("./emailsDbConnect.js");
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.set("views", "./views");
+app.set("view engine", "pug");
 
-app.use(express.static('public'));
-app.use(bodyParser.json())
+app.use(express.static("public"));
+app.use(bodyParser.json());
 
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
   numOfUsers((users) => {
     res.send({ users });
   });
 });
 
-app.get('/remove/:emailId', (req, res) => {
+app.get("/remove/:emailId", (req, res) => {
   const emailIdToRemove = req.params.emailId;
   removeEmailAddress(emailIdToRemove, () => {
-    res.render('unsubscribed')
-  })
-})
-
-app.get('/verify/:emailId', (req, res) => {
-  const emailIdToVerify = req.params.emailId;
-  verifyEmailAddress(emailIdToVerify, () => {
-    res.render('verified');
-  })
-})
-
-
-app.post('/send', (req, res) => {
-  let subject = req.body.subject;
-  let body = req.body.body;
-  selectEmailAddress((recipient) => {
-    send(recipient.email, recipient.identifier, subject, body, false)
-    res.send('email has been sent from /sent');
+    res.render("unsubscribed");
   });
 });
 
-app.post('/email', (req, res) => {
+app.get("/verify/:emailId", (req, res) => {
+  const emailIdToVerify = req.params.emailId;
+  verifyEmailAddress(emailIdToVerify, () => {
+    res.render("verified");
+  });
+});
+
+app.post("/send", (req, res) => {
+  let subject = req.body.subject;
+  let body = req.body.body;
+  selectEmailAddress((recipient) => {
+    send(recipient.email, recipient.identifier, subject, body, false);
+    res.send("email has been sent from /sent");
+  });
+});
+
+app.post("/email", (req, res) => {
   let addToEmailList = req.body.email;
   let emailList = {};
   saveEmail(addToEmailList, (document) => {
-    if (document === 'ALREADY ADDED') {
-      emailList.status = 'ALREADY SIGNED UP!';
-      res.send(emailList)
+    if (document === "ALREADY ADDED") {
+      emailList.status = "ALREADY SIGNED UP!";
+      res.send(emailList);
     } else {
-      send(document.email, document.identifier, 'Welcome to heatMail - Verify Your Email Address','', true);
-      emailList.status = 'ADDED!';
+      send(
+        document.email,
+        document.identifier,
+        "Welcome to heatMail - Verify Your Email Address",
+        "",
+        true
+      );
+      emailList.status = "ADDED!";
       res.send(emailList);
     }
-  })
+  });
 });
-
 
 app.listen(3600, () => {
   console.log(`listening on 3600`);
 });
-

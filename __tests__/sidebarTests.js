b3c9e36c5ad76beeea68ee5de5262e41676cbc59
Jesse Bell <jkurtasbell@gmail.com>
2020-11-20 13:03:10 -0800

    Add Snapshot Tests

--- __tests__/sidebarTests.js
+++ __tests__/sidebarTests.js
@@ -2,3 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Sidebar from '../public/components/Sidebar.jsx';
+
+test('preview-snapshot-test', () => {
+  const component = renderer.create(
+    <Sidebar/>);
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    console.log('tree------c.p', tree.children[0].props);
+})
--- __tests__/signUpTests.js
+++ __tests__/signUpTests.js
@@ -3,3 +3,12 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Signup from '../public/components/Signup.jsx';
 
+test('preview-snapshot-test', () => {
+  const component = renderer.create(
+    <Signup/>);
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    console.log('tree------c.p', tree.children[0].props);
+})

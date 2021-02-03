2eb55b2f823eed91a4086aba5a237ff5cc9c1f43
Jesse Bell <jkurtasbell@gmail.com>
2021-02-03 14:38:03 -0800

    Adjust Rules in Sidebar Componenet

--- public/components/Sidebar.jsx
+++ public/components/Sidebar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="rules">
       <div className="rules-title">IMPORTANT</div>
-      <div className="rules">
+      <div className="rules-listed">
         Do <strong>NOT</strong> use or include: <br />
         &#8226; personal information <br />
         &#8226; names, locations, etc. <br />
-        &#8226; bigoted language of any kind <br />
-        &#8226; threatening, violent language <br />
+        &#8226; bigoted language <br />
+        &#8226; violent language <br />
+        &#8226; threatening language <br />
+
       </div>
     </div>
   );

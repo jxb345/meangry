d0ca75f4ca7b887c7d86ea532dd38eff77e06c63
Jesse Bell <jkurtasbell@gmail.com>
2021-02-22 14:46:22 -0800

    Add Error Icon

--- public/components/Sidebar.jsx
+++ public/components/Sidebar.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="rules">
-      <div className="rules-title">IMPORTANT</div>
+      <div className="rules-title">
+        <img src="./error.png" height="30" width="30" alt=""/>
+        IMPORTANT</div>
       <div className="rules-listed">
         {/* Do <strong>NOT</strong> use or include: <br /> */}
         {/* &#8226; personal information <br /> */}
-        NO names, locations <br />
-        NO bigoted language <br />
-        NO violent language <br />
+        &#8226; NO names, locations <br />
+        &#8226; NO bigoted language <br />
+        &#8226; NO violent language <br />
         {/* &#8226; threatening language <br /> */}
 
       </div>

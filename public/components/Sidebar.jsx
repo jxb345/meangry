4836913bc91ca480db2a20246dbc42cd93b87b53
Jesse Bell <jkurtasbell@gmail.com>
2021-02-22 23:57:30 -0800

    Reconfigure Sidebar.jsx

--- public/components/Sidebar.jsx
+++ public/components/Sidebar.jsx
@@ -4,14 +4,15 @@ const Sidebar = () => {
   return (
     <div className="rules">
       <div className="rules-title">
-        <img src="./error.png" height="30" width="30" alt=""/>
-        IMPORTANT</div>
+        <img className="error-img" src="./warning-64.png" height="35" width="35" alt=""/>
+      </div>
       <div className="rules-listed">
         {/* Do <strong>NOT</strong> use or include: <br /> */}
         {/* &#8226; personal information <br /> */}
-        &#8226; NO names, locations <br />
+        {/* &#8226; NO names, locations <br />
         &#8226; NO bigoted language <br />
-        &#8226; NO violent language <br />
+        &#8226; NO violent language <br /> */}
+        No names, locations, bigoted or violent language.
         {/* &#8226; threatening language <br /> */}
 
       </div>

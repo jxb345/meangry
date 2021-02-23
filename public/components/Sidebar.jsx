fe2bb26742e705e1831a053f45011f80a3b60733
Jesse Bell <jkurtasbell@gmail.com>
2021-02-23 14:24:33 -0800

    Modify Positioning Of Sidebar.jsx

--- public/components/Sidebar.jsx
+++ public/components/Sidebar.jsx
@@ -7,14 +7,7 @@ const Sidebar = () => {
         <img className="error-img" src="./warning-64.png" height="35" width="35" alt=""/>
       </div>
       <div className="rules-listed">
-        {/* Do <strong>NOT</strong> use or include: <br /> */}
-        {/* &#8226; personal information <br /> */}
-        {/* &#8226; NO names, locations <br />
-        &#8226; NO bigoted language <br />
-        &#8226; NO violent language <br /> */}
-        No names, locations, bigoted or violent language.
-        {/* &#8226; threatening language <br /> */}
-
+        NO names, locations, bigoted or violent language.
       </div>
     </div>
   );
